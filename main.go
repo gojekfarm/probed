636e27ed2f5a02e22311bd3cb8293f0745768aca
Rajeev N B <rajeevrvis@gmail.com>
2018-03-05 00:38:08 +0530

    accept workerCount from command line args with default value

--- main.go
+++ main.go
@@ -14,8 +14,9 @@ var kongAdminPort = flag.String("kong-admin-port", "8001", "kong admin port")
 
 var healthCheckInterval = flag.String("health-check-interval", "2000", "healt check interval in ms")
 var healthCheckPath = flag.String("health-check-path", "/ping", "path to check for active health check")
-
 var healthCheckType = flag.String("health-check-type", "tcp", "supports http or tcp checks")
+
+var workerCount = flag.Int("worker-count", 100, "no of workers which participate in healthcheck of targets")
 
 func main() {
 	flag.Parse()
@@ -51,9 +52,8 @@ func main() {
 		healthCheckType: *healthCheckType,
 	}
 
-	// TODO: accept workerCount from command line args
 	wm := workerManager{
-		workerCount: 100,
+		workerCount: *workerCount,
 		jobFn:       p.start,
 	}
 
